62d22dcd77edeae8dff6e0b92eec2325e4c46361
muhammadmukarram <mukarramsardar@outlook.com>
2016-04-20 16:11:55 +0500

    Mukarram 2
    Updated By Mukarram

---
+++ Warid/Controllers/mobileinternetsharingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Warid.Controllers
+{
+    public class mobileinternetsharingController : Controller
+    {
+        // GET: mobileinternetsharing
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
